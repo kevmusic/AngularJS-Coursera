11f9b0041f2ee703c47358fc910f5ba4e2463565
Kevin Moreira <kevmusic@kECMbP.local>
2016-09-29 00:55:15 -0600

    Last Week 2 update

---
+++ Week 2/12- CS with .provider()/app.js
@@ -0,0 +1,78 @@
+(function () {
+    'use strict';
+    console.clear();
+    
+    angular.module('ControllerAsApp', [])
+        .controller('ShoppingListController', ShoppingListController)
+        .provider('ShoppingListService', ShoppingListServiceProvider)
+        .config(Config);
+    
+    Config.$inject = ['ShoppingListServiceProvider'];
+    function Config(ShoppingListServiceProvider) {
+        ShoppingListServiceProvider.defaults.maxItems = 2;
+    }
+    
+    ShoppingListController.$inject = ['ShoppingListService'];
+    function ShoppingListController(ShoppingListService) {
+        var list = this;
+        
+        list.items = ShoppingListService.getItems();
+        
+        list.itemName = "";
+        list.itemQuantity = "";
+        
+        list.addItem = function() {
+            try {
+                ShoppingListService.addItem(list.itemName, list.itemQuantity);
+            }
+            catch (error) {
+                list.errorMessage = error.message;
+            }
+            
+        };
+    }
+    
+    function ShoppingListService(maxItems) {
+        var service = this;
+        
+        var items = [];
+        
+        service.addItem = function (itemName, quantity) {
+            if ( (maxItems === undefined) ||
+                 (maxItems !== undefined) && (items.length < maxItems) ) {
+                var item = {
+                    name: itemName,
+                    quantity: quantity
+                };
+                
+                items.push(item);
+            }
+            else {
+                throw new Error("Max items (" + maxItems + ") reached.");
+            }
+        }
+        
+        service.removeItem = function (itemIndex) {
+            items.splice(itemIndex, 1);
+        }
+
+        service.getItems = function() {
+            return items;
+        }
+    }
+    
+    function ShoppingListServiceProvider() {
+        var provider = this;
+        
+        provider.defaults = {
+            maxItems: 10
+        }
+        
+        provider.$get = function() {
+            var shoppingList = new ShoppingListService(provider.defaults.maxItems);
+            
+            return shoppingList;
+        };
+    }
+
+})();
